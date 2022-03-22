631902283cc2e15d9d1063b9a1f9406126c309e0
Laurent Bernabe <laurent.bernabe@gmail.com>
2022-03-22 15:37:17 +0100

    You override properties instead of css variables

--- chessboard.js
+++ chessboard.js
@@ -4,10 +4,8 @@ import {LitElement, html, css} from 'lit';
  * A chessboard component
  * 
  * CSS variables :
- * --size: (String) size of the chess board, in whatever unit you want. Defaults to 100px.
- * --backgroundColor: (String) color of the background. Defaults to pink.
- * @fires count-changed - Indicates when the count changes
- * @csspart button - The button
+ * size: (String) size of the chess board, in whatever unit you want. Defaults to 100px.
+ * backgroundColor: (String) color of the background. Defaults to pink.
  */
 export class ChessBoard extends LitElement {
   static get styles() {
@@ -16,8 +14,8 @@ export class ChessBoard extends LitElement {
         --defaultSize: 100px;
         --defaultBackgroundColor: pink;
         display: grid;
-        width: var(--size, var(--defaultSize));
-        height: var(--size, var(--defaultSize));
+        width: var(--size);
+        height: var(--size);
         background-color: var(--backgroundColor, var(--defaultBackgroundColor));
       }
     `;
@@ -31,6 +29,26 @@ export class ChessBoard extends LitElement {
        */
       size: {type: String},
     };
+  }
+
+  constructor() {
+    super();
+    this._defaultSize = '100px';
+  }
+
+  updated(changes) {
+    if (changes.has('size')) {
+      const groups = this.size.match(/([0-9\.]+)(.*)/);
+      const sizeValue = parseFloat(groups[1]);
+      const unit = groups[2];
+      const halfSizeValue = sizeValue / 2.0;
+      this.style.setProperty('--size', this.size);
+      this.style.setProperty('--half-size', `${halfSizeValue}${unit}`);
+    }
+    else {
+      this.style.setProperty('--size', '100px');
+      this.style.setProperty('--half-size', '50px');
+    }
   }
 
   render() {
