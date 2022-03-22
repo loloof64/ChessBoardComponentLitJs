957f7438bb48c293b2c327d95649533d68647b1f
Laurent Bernabe <laurent.bernabe@gmail.com>
2022-03-22 17:11:45 +0100

    We can toggle board orientation, and fixed fact that default values were not handled correctly

--- chessboard.js
+++ chessboard.js
@@ -1,11 +1,7 @@
 import {LitElement, html, css} from 'lit';
 
 /**
- * A chessboard component
- * 
- * CSS variables :
- * size: (String) size of the chess board, in whatever unit you want. Defaults to 100px.
- * backgroundColor: (String) color of the background. Defaults to pink.
+ * A chessboard component.
  */
 export class ChessBoard extends LitElement {
   static get styles() {
@@ -39,6 +35,13 @@ export class ChessBoard extends LitElement {
        * @type {String}
        */
       size: {type: String},
+
+      /**
+       * Is the black side at the bottom of the board ? Defaults to false.
+       * @type {Boolean}
+       */
+      reversed: {type: Boolean},
+
       /**
        * The background color. Defaults to pink.
        * @type {String}
@@ -55,7 +58,19 @@ export class ChessBoard extends LitElement {
 
   constructor() {
     super();
-    this._defaultSize = '100px';
+
+    this.style.setProperty('--backgroundColor', 'pink');
+    this.style.setProperty('--coordinates-color', 'yellow');
+
+    this.style.setProperty('--size', '100px');
+    this.style.setProperty('--cells-size', '50px');
+    const cellsSize = 50 / 9.0;
+    const halfCellsSize = cellsSize / 2.0;
+    const coordinatesFontSize = cellsSize * 0.00125;
+    this.style.setProperty('--grid-template', `${halfCellsSize}px repeat(8, ${cellsSize}px) ${halfCellsSize}px / ${halfCellsSize}px repeat(8, ${cellsSize}px) ${halfCellsSize}px`);
+    this.style.setProperty('--coordinates-font-size', `${coordinatesFontSize}px`);
+
+
   }
 
   _rowCells(rowIndex) {
@@ -84,28 +99,13 @@ export class ChessBoard extends LitElement {
       this.style.setProperty('--grid-template', `${halfCellsSize}${unit} repeat(8, ${cellsSize}${unit}) ${halfCellsSize}${unit} / ${halfCellsSize}${unit} repeat(8, ${cellsSize}${unit}) ${halfCellsSize}${unit}`);
       this.style.setProperty('--coordinates-font-size', `${coordinatesFontSize}${unit}`);
     }
-    else {
-      this.style.setProperty('--size', '100px');
-      this.style.setProperty('--cells-size', '50px');
-      const cellsSize = 50 / 9.0;
-      const halfCellsSize = cellsSize / 2.0;
-      const coordinatesFontSize = cellsSize * 0.00125;
-      this.style.setProperty('--grid-template', `${halfCellsSize}px repeat(8, ${cellsSize}px) ${halfCellsSize}px / ${halfCellsSize}px repeat(8, ${cellsSize}px) ${halfCellsSize}px`);
-      this.style.setProperty('--coordinates-font-size', `${coordinatesFontSize}px`);
-    }
 
     if (changes.has('backgroundColor')) {
       this.style.setProperty('--backgroundColor', this.backgroundColor);
     }
-    else {
-      this.style.setProperty('--backgroundColor', 'pink');
-    }
 
     if (changes.has('coordinates-color')) {
       this.style.setProperty('--coordinates-color', this.coordinatesColor);
-    }
-    else {
-      this.style.setProperty('--coordinates-color', 'yellow');
     }
   }
 
@@ -113,29 +113,29 @@ export class ChessBoard extends LitElement {
     return html`
       <div class="lower_layer">
         <div></div>
-        <div class="coordinate">A</div>
-        <div class="coordinate">B</div>
-        <div class="coordinate">C</div>
-        <div class="coordinate">D</div>
-        <div class="coordinate">E</div>
-        <div class="coordinate">F</div>
-        <div class="coordinate">G</div>
-        <div class="coordinate">H</div>
+        <div class="coordinate">${this.reversed ? 'H' : 'A'}</div>
+        <div class="coordinate">${this.reversed ? 'G' : 'B'}</div>
+        <div class="coordinate">${this.reversed ? 'F' : 'C'}</div>
+        <div class="coordinate">${this.reversed ? 'E' : 'D'}</div>
+        <div class="coordinate">${this.reversed ? 'D' : 'E'}</div>
+        <div class="coordinate">${this.reversed ? 'C' : 'F'}</div>
+        <div class="coordinate">${this.reversed ? 'B' : 'G'}</div>
+        <div class="coordinate">${this.reversed ? 'A' : 'H'}</div>
         <div></div>
 
         ${
-          [8,7,6,4,5,3,2,1].map((row) => this._rowCells(row))
+          (this.reversed ? [1,2,3,4,5,6,7,8] : [8,7,6,4,5,3,2,1]).map((row) => this._rowCells(row))
         }
 
         <div></div>
-        <div class="coordinate">A</div>
-        <div class="coordinate">B</div>
-        <div class="coordinate">C</div>
-        <div class="coordinate">D</div>
-        <div class="coordinate">E</div>
-        <div class="coordinate">F</div>
-        <div class="coordinate">G</div>
-        <div class="coordinate">H</div>
+        <div class="coordinate">${this.reversed ? 'H' : 'A'}</div>
+        <div class="coordinate">${this.reversed ? 'G' : 'B'}</div>
+        <div class="coordinate">${this.reversed ? 'F' : 'C'}</div>
+        <div class="coordinate">${this.reversed ? 'E' : 'D'}</div>
+        <div class="coordinate">${this.reversed ? 'D' : 'E'}</div>
+        <div class="coordinate">${this.reversed ? 'C' : 'F'}</div>
+        <div class="coordinate">${this.reversed ? 'B' : 'G'}</div>
+        <div class="coordinate">${this.reversed ? 'A' : 'H'}</div>
         <div></div>
 
       </div>
