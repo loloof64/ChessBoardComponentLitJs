a9e086e01e50f3ec9db2e455bb3e78d5601e8c00
Laurent Bernabe <laurent.bernabe@gmail.com>
2022-03-22 14:41:51 +0100

    We can change the background color.

--- chessboard.js
+++ chessboard.js
@@ -2,20 +2,23 @@ import {LitElement, html, css} from 'lit';
 
 /**
  * A chessboard component
- *
+ * 
+ * CSS variables :
+ * --size: (String) size of the chess board, in whatever unit you want. Defaults to 100px.
+ * --backgroundColor: (String) color of the background. Defaults to pink.
  * @fires count-changed - Indicates when the count changes
- * @slot - This element has a slot
  * @csspart button - The button
  */
 export class ChessBoard extends LitElement {
   static get styles() {
     return css`
       :host {
-        --defaultSize: 10px;
-        display: inline-flex;
+        --defaultSize: 100px;
+        --defaultBackgroundColor: pink;
+        display: grid;
         width: var(--size, var(--defaultSize));
         height: var(--size, var(--defaultSize));
-        background-color: pink;
+        background-color: var(--backgroundColor, var(--defaultBackgroundColor));
       }
     `;
   }
